146730e67f26b7fad917079d8a35d3e2a1be19b7
Philipp Klaus <philipp.klaus@gmail.com>
2017-08-27 12:28:24 +0200

    new kwarg `line_ending=` for .query() .write() .read()

--- universal_usbtmc/instrument.py
+++ universal_usbtmc/instrument.py
@@ -58,30 +58,34 @@ class Instrument(object):
         """
         raise NotImplementedError()
 
-    def write(self, message, encoding='default'):
+    def write(self, message, encoding='default', line_ending='default'):
         """
         Send a string message to the instrument
 
         :param str message: The message to send
         :param str encoding: The encoding to use when converting the message from str to bytes
+        :param str line_ending: The line ending to add to the message
         """
         encoding = self.ENCODING if encoding == 'default' else encoding
+        line_ending = self.LINE_ENDING if line_ending == 'default' else line_ending
         if type(message) in (tuple, list):
             # recursive call for a list of commands
             for message_i in message:
                 self.write(message_i, encoding)
             return
-        self.write_raw((str(message)+self.LINE_ENDING).encode(encoding))
+        self.write_raw((str(message)+line_ending).encode(encoding))
 
-    def read(self, num=-1, encoding='default'):
+    def read(self, num=-1, encoding='default', line_ending='default'):
         """
         Read a response string from the instrument
 
         :param bytes num: Number of bytes to read back
         :param str encoding: The encoding to use when converting the response from bytes to str
+        :param str line_ending: The line ending to strip off from end of received message
         """
         encoding = self.ENCODING if encoding == 'default' else encoding
-        return self.read_raw(num).decode(encoding).rstrip('\r\n')
+        line_ending = self.LINE_ENDING if line_ending == 'default' else line_ending
+        return self.read_raw(num).decode(encoding).rstrip(line_ending)
 
     def query_raw(self, message, encoding='default', num=-1):
         """
@@ -92,21 +96,22 @@ class Instrument(object):
         self.write(message)
         return self.read_raw(num)
 
-    def query(self, message, num=-1, encoding='default'):
+    def query(self, message, num=-1, encoding='default', line_ending='default'):
         """
         Convenience method to first send a string command,
         then read and return a response string.
+        :param str line_ending: The line ending to add to the message
         """
         encoding = self.ENCODING if encoding == 'default' else encoding
         if type(message) is tuple or type(message) is list:
             # recursive call for a list of commands
             val = list()
             for message_i in message:
-                val.append(self.query(message_i, num=num, encoding=encoding))
+                val.append(self.query(message_i, num=num, encoding=encoding, line_ending=line_ending))
             return val
 
-        self.write(message, encoding=encoding)
-        return self.read(num, encoding=encoding)
+        self.write(message, encoding=encoding, line_ending=line_ending)
+        return self.read(num, encoding=encoding, line_ending=line_ending)
 
     @property
     def idn(self):
